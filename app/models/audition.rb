d8a31420f6f1a3ad456714719e8029056f15a7d1
Alex Manzi <alex.manzi22@gmail.com>
2022-03-21 20:12:11 -0400

    commit tree 9a58d6f4dd57380ff673ac60a86f8b7046340419

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,8 +1,11 @@
 class Audition < ActiveRecord::Base
-    belongs_to :role
+    belong_to :role
+
+    def role
+        self.role
+    end
 
     def call_back
-        self.hired = true;
-        self.save
+        self.hired = true
     end
 end
